ce9632847c63baf77fe01b544a6f7f41467fee32
Saurabh Zend <Saurabh Zend@SAURABH>
2023-03-28 11:00:24 +0530

    second commit

--- FlipkartQA/src/test/java/setup/Base.java
+++ FlipkartQA/src/test/java/setup/Base.java
@@ -3,6 +3,7 @@ package setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class Base {
 	
@@ -17,6 +18,12 @@ public class Base {
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Saurabh Zend\\eclipse-workspace\\FlipkartQA\\browser\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+	public static WebDriver openoperaBrowser() 
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Saurabh Zend\\eclipse-workspace\\FlipkartQA\\browser\\chromedriver.exe");
+		WebDriver driver = new OperaDriver();
 		return driver;
 	}
 
